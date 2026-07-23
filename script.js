38de0687996ecd238f4735dbecb5d366a17ecfa3
Marwiclothcollection <shanihunyar786003@gmail.com>
2026-07-24 02:19:13 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+const searchBox = document.getElementById("searchBox");
 
+searchBox.addEventListener("keyup", function () {
+
+let filter = searchBox.value.toLowerCase();
+
+let products = document.querySelectorAll(".product-card");
+
+products.forEach(function(product){
+
+let title = product.querySelector("h3").innerText.toLowerCase();
+
+if(title.includes(filter)){
+product.style.display="block";
+}
+else{
+product.style.display="none";
+}
+
+});
+
+});
